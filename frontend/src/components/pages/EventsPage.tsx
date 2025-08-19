44dbc276a21b2a0af00ea0fe4eb2c283edea2d7d
Sylvia Suet Wai Yung <sylvia.yung@ntnu.no>
2025-08-19 17:58:49 +0200

    update eventspage to INewEvent

--- frontend/src/components/pages/EventsPage.tsx
+++ frontend/src/components/pages/EventsPage.tsx
@@ -3,7 +3,7 @@ import { fetchEventsByStartDate } from "../../api/owApi";
 import { EventCard } from "../cards/EventCard";
 import { Loading } from "../utils/Loading";
 import { Error } from "../utils/Error";
-import { IEvent } from "../../lib/types";
+import { INewEvent } from "../../lib/types";
 
 const REFETCH_INTERVAL_MINUTES = 5; // how often to refetch events from Online API
 const NUMBER_OF_EVENTS = 8; // how many events to display
@@ -15,13 +15,17 @@ export const EventsPage = () => {
     refetchInterval: 1000 * 60 * REFETCH_INTERVAL_MINUTES,
   });
 
+  console.log("Events API response:", data);
+
+
   if (isLoading) return <Loading text="Henter siste arrangementer fra OW..." />;
   if (isError) return <Error />;
 
   // Filter and limit events before rendering
   const now = new Date();
-  const filteredEvents = data?.results
-    .filter((event: IEvent) => new Date(event.end_date) > now) // Filter out past events
+  const filteredEvents = data
+    ?.filter((event: INewEvent) => new Date(event.end) > now) // Filter out past events
+    .sort((a: INewEvent, b: INewEvent) => new Date(a.start).getTime() - new Date(b.start).getTime())
     .slice(0, NUMBER_OF_EVENTS); // Limit to NUMBER_OF_EVENTS
 
   return (
@@ -30,7 +34,7 @@ export const EventsPage = () => {
         Kommende arrangementer
       </div>
       <div className="grid justify-between max-w-full grid-cols-4 gap-8">
-        {filteredEvents?.map((event: IEvent) => (
+        {(filteredEvents ?? []).map((event: INewEvent) => (
           <EventCard key={event.id} event={event} />
         ))}
       </div>
